7db946c249625128b498ec57fdb5c88fba1b8ef9
nouman2005 <m.nouman2005@gmail.com>
2025-12-21 14:07:49 +0530

    Setup Frontend and Backend both

---
+++ Backend/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv, { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+import ConnectDB from "./Config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://192.168.1.34:4000", "http://localhost:5173"],
+    credentials: true,
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("<h1>SuccessFully Connected</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`✅ Server Running At Port ${process.env.PORT}`);
+});
